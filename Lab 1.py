2353e48bcc37b7d72034766f823688c2f848e5f0
Jason Green <jgreen44@asu.edu>
2019-08-11 19:21:37 -0500

    Initial commit

---
+++ Lab 1.py
@@ -0,0 +1,43 @@
+#   Filename:           Lab 1
+#   Created by:         jasongreen
+#   Date:               Tuesday, January 08, 2019
+#   Time of Creation:   11:09
+#   ---
+
+# print("Hello!")
+# print("This is my first python program!")
+
+x = 10
+print(x)
+
+y = 15
+print(y)
+
+z = 0.7
+print(z)
+
+
+course_name = "Introduction to Programming Logic"
+print(course_name)
+
+
+
+a = True
+b = False
+
+sum = a or b
+print(sum)
+
+na = not a
+print(na)
+
+
+
+
+
+
+
+
+
+
+
